3167d50c0a8bc695cc693066e3b9c0a7538a7ebe
ChihaoDEV2025 <chihao.dev@gmail.com>
2025-04-17 16:38:02 +0700

    JS-Advance : revising -> Fetch and try to fetch JSON file

---
+++ Coursera/JS_Advance/3_Fetch_JsonFIle/Employees.js
@@ -0,0 +1,22 @@
+async function fetchEmployees() {
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!response.ok) {
+      return new Error("Cannot get the response!!");
+    }
+
+    let data = await response.json();
+    display(data);
+  } catch (error) {
+    console.log("Have an error : " + error);
+  }
+}
+
+function display(data) {
+  data.forEach((employee) => {
+    console.log(`${employee.id} - ${employee.name}`);
+  });
+}
+
+//main function
+fetchEmployees();
